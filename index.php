1d8bbb34a29b4afd313b761da05aad2c46cc8d15
cochegar <cochegar@mail.ru>
2021-02-08 22:35:46 +0300

    start vue

--- index.php
+++ index.php
@@ -13,9 +13,32 @@ function func($number)
 
      return $str;
  }
- echo test('test');
-echo func(10);
-$a=test('123');
-$b=test('123');
+ //echo test('test');
+//echo func(10);
 
+
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+
+
+</head>
+<body>
+<div id="app-4">
+    <ol>
+        <li v-for="todo2 in todos">
+            {{ todo2.text }}
+        </li>
+    </ol>
+</div>
+<div id="app">
+  {{ text }}
+</div>
+<script src="myvue.js"></script>
+</body>
+</html>';
 ?>
---
+++ myvue.js
@@ -0,0 +1,21 @@
+new Vue(
+    {
+      el: '#app-4',
+      data:  {
+        todos: [
+          {text: 'Изучить JavaScript'},
+          {text: 'Изучить Vue'},
+          {text: 'Создать что-нибудь классное'},
+          {text: 'Vuejs'}
+        ]
+      }
+    });
+new Vue(
+    {
+      el: '#app',
+      data: {text: 'Привет, Vue!'}
+    },
+
+
+
+)
